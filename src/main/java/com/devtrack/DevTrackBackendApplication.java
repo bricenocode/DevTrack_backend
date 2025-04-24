1afa5b9ec144e537df9698baec042040427c0ee3
bricenodev <miguelbricenocode@gmail.com>
2025-04-24 11:31:51 +0200

    build(config):Setting a .env file to have more security about our application.properties.

--- src/main/java/com/devtrack/DevTrackBackendApplication.java
+++ src/main/java/com/devtrack/DevTrackBackendApplication.java
@@ -1,5 +1,6 @@
 package com.devtrack;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevTrackBackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        //Cargamos las variables de entorno por seguridad
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(DevTrackBackendApplication.class, args);
     }
 
